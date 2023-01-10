eab1391e57bc6b601e6178b372cfadaadc0f625e
Vika Shleina <vsh@nyantec.com>
2023-01-10 16:23:18 +0300

    Factor out fetching client certificate DN
    This will allow us to replace the implementation later, if the need
    arises, or use it in other parts (such as detecting admin users).

--- src/axum.rs
+++ src/axum.rs
@@ -17,32 +17,69 @@ Hint: if you use nginx, use:
 to provide the necessary headers.
 ";
 
+pub struct CertDn(String);
+impl CertDn {
+	/// Parse the UID out of a client certificate DN.
+	pub fn uid(&self) -> Option<&str> {
+		DN_UID_REGEX.captures(&self.0)
+			.map(|c| c.get(1).unwrap().as_str())
+	}
+}
+
+#[async_trait::async_trait]
+impl<T> FromRequestParts<T> for CertDn where
+    T: Send + Sync
+{
+    type Rejection = CertDnExtractionError;
+
+    async fn from_request_parts(parts: &mut Parts, _: &T) -> Result<Self, Self::Rejection> {
+		match parts
+			.headers
+			.get("X-SSL-Verify")
+			.map(|s| String::from_utf8_lossy(s.as_bytes()))
+			.as_deref()
+		{
+			Some("SUCCESS") => {}
+			// We have no client certificate
+			Some("NONE") => return Err(CertDnExtractionError::NoTlsCert),
+			// Client certificate validation failed (e.g. it was revoked)
+			Some(failed) => return Err(CertDnExtractionError::CertValidationFailed(failed.to_owned())),
+			None => {
+				#[cfg(debug_assertions)]
+				return Ok(CertDn("O = nyantec GmbH, CN = Vika Shleina, GN = Viktoriya, SN = Shleina, pseudonym = Vika, UID = vsh".to_string()));
+				#[cfg(not(debug_assertions))]
+				return Err(CertDnExtractionError::ReverseProxyMisconfigured);
+			}
+		}
+		if let Some(ssl_client_s_dn) = parts
+			.headers
+			.get("X-SSL-Client-Dn")
+			.map(|s| String::from_utf8_lossy(s.as_bytes()))
+		{
+			return Ok(CertDn(ssl_client_s_dn.to_string()))
+		} else {
+			return Err(CertDnExtractionError::ReverseProxyMisconfigured);
+		}
+    }
+}
+
 #[derive(Debug, thiserror::Error)]
-pub enum UserExtractionError {
-	#[error("SQL layer error: {0}")]
-	Sql(#[from] sqlx::Error),
-	#[error("User not found in database")]
-	UserNotFound,
+pub enum CertDnExtractionError {
 	#[error("No TLS client certificate was provided")]
 	NoTlsCert,
 	#[error("Certificate validation {0}")]
 	CertValidationFailed(String),
 	#[error("Required headers `X-SSL-Verify` and/or `X-SSL-Client-Dn` not found")]
 	ReverseProxyMisconfigured,
-	#[error("No UID field in TLS client certificate's Subject DN")]
-	NoUidFieldInCert,
 }
 
-impl IntoResponse for UserExtractionError {
+impl IntoResponse for CertDnExtractionError {
 	fn into_response(self) -> Response {
 		(
 			match &self {
-				UserExtractionError::Sql(err) => StatusCode::INTERNAL_SERVER_ERROR,
-				UserExtractionError::UserNotFound => StatusCode::UNAUTHORIZED,
-				UserExtractionError::NoTlsCert => StatusCode::UNAUTHORIZED,
-				UserExtractionError::CertValidationFailed(_) => StatusCode::FORBIDDEN,
-				UserExtractionError::ReverseProxyMisconfigured => StatusCode::INTERNAL_SERVER_ERROR,
-				UserExtractionError::NoUidFieldInCert => StatusCode::BAD_REQUEST,
+				Self::CertValidationFailed(_) => StatusCode::FORBIDDEN,
+				Self::ReverseProxyMisconfigured => StatusCode::INTERNAL_SERVER_ERROR,
+				Self::NoTlsCert => StatusCode::UNAUTHORIZED,
 			},
 			[("Content-Type", "text/plain")],
 			match &self {
@@ -54,53 +91,58 @@ impl IntoResponse for UserExtractionError {
 	}
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum UserExtractionError {
+	#[error("SQL layer error: {0}")]
+	Sql(#[from] sqlx::Error),
+	#[error("User not found in database")]
+	UserNotFound,
+	#[error("No UID field in TLS client certificate's Subject DN")]
+	NoUidFieldInCert,
+	#[error("Error parsing TLS client certificate data")]
+	Certificate(#[from] CertDnExtractionError)
+}
+
+impl IntoResponse for UserExtractionError {
+	fn into_response(self) -> Response {
+		if let Self::Certificate(err) = self {
+			return err.into_response();
+		}
+
+		(
+			match &self {
+				Self::Sql(_) => StatusCode::INTERNAL_SERVER_ERROR,
+				Self::UserNotFound => StatusCode::UNAUTHORIZED,
+				Self::NoUidFieldInCert => StatusCode::BAD_REQUEST,
+				_ => unreachable!()
+			},
+			[("Content-Type", "text/plain")],
+			self.to_string(),
+		)
+			.into_response()
+	}
+}
+
 #[async_trait::async_trait]
 impl FromRequestParts<Arc<Service<MigrationsDone>>> for User {
 	type Rejection = UserExtractionError;
 
 	async fn from_request_parts(parts: &mut Parts, db: &Arc<Service<MigrationsDone>>) -> Result<Self, Self::Rejection> {
-		match parts
-			.headers
-			.get("X-SSL-Verify")
-			.map(|s| String::from_utf8_lossy(s.as_bytes()))
-			.as_deref()
-		{
-			Some("SUCCESS") => {}
-			// We have no client certificate
-			Some("NONE") => return Err(UserExtractionError::NoTlsCert),
-			// Client certificate validation failed (e.g. it was revoked)
-			Some(failed) => return Err(UserExtractionError::CertValidationFailed(failed.to_owned())),
-			None => {
-				#[cfg(debug_assertions)]
-				return Ok(db.find_user_by_name("vsh").await?.unwrap());
-				#[cfg(not(debug_assertions))]
-				return Err(UserExtractionError::ReverseProxyMisconfigured);
-			}
-		}
-		if let Some(ssl_client_s_dn) = parts
-			.headers
-			.get("X-SSL-Client-Dn")
-			.map(|s| String::from_utf8_lossy(s.as_bytes()))
-		{
-			// XXX: This only supports three-letter usernames!
-			// A proper RFC4514-compliant parser is recommended
-			if let Some(captures) = DN_UID_REGEX.captures(ssl_client_s_dn.as_ref()) {
-				let username = captures.get(1).unwrap().as_str();
-				tracing::debug!("UID from X-SSL-Client-Dn: {}", username);
-
-				match db.find_user_by_name(username).await {
+		match CertDn::from_request_parts(parts, db).await {
+			Ok(cert_dn) => match cert_dn.uid() {
+				Some(uid) => match db.find_user_by_name(uid).await {
 					Ok(Some(user)) => return Ok(user),
 					Ok(None) => return Err(UserExtractionError::UserNotFound),
 					Err(err) => return Err(UserExtractionError::Sql(err)),
-				}
-			} else {
-				return Err(UserExtractionError::NoUidFieldInCert);
+				},
+				None => return Err(UserExtractionError::NoUidFieldInCert)
+			},
+			Err(err) => {
+				#[cfg(debug_assertions)]
+				return Ok(db.find_user_by_name("vsh").await?.unwrap());
+				#[cfg(not(debug_assertions))]
+				return Err(err.into());
 			}
-		} else {
-			#[cfg(debug_assertions)]
-			return Ok(db.find_user_by_name("vsh").await?.unwrap());
-			#[cfg(not(debug_assertions))]
-			return Err(UserExtractionError::ReverseProxyMisconfigured);
 		}
 	}
 }
