5bd1a3ea97ebe3b8ce09bcb90e773b4f47bc7f08
Vika Shleina <vsh@nyantec.com>
2023-01-31 22:15:12 +0300

    nixos-test: Test mail delivery to users with Dovecot
    This is more of a black-box test. We could also verify that the
    mailbox of the user with the corresponding UUID exists in the
    filesystem, to be sure that Dovecot is not doing something weird.

--- nixos-test.nix
+++ nixos-test.nix
@@ -7,7 +7,15 @@
 # exec("\n".join(driver.tests.splitlines()[0:13]))
 # ```
 self: pkgs:
-{ lib, ... }: {
+{ lib, nodes, ... }: let
+  test-email = pkgs.writeText "test-email" ''
+    From: root@localhost
+    To: vsh@localhost
+    Subject: test email to user
+
+    This is a test email to a user.
+  '';
+in {
   name = "nixos-mail-passwd";
   hostPkgs = pkgs;
 
@@ -37,6 +45,10 @@ self: pkgs:
 
       # We disable nginx since we'll access the service directly
       services.nginx.enable = lib.mkForce false;
+
+      system.extraDependencies = [
+        test-email
+      ];
     };
   };
 
@@ -55,7 +67,24 @@ self: pkgs:
         password = server.succeed(f"curl --silent --fail -H 'X-Ssl-Verify: SUCCESS' -H 'X-Ssl-Client-Dn: {vsh}' -d label=test -d expires_in=noexpiry http://localhost:3000/create_password | grep -o '<code>[^<]*</code>' | cut -b7- | cut -d'<' -f1").strip()
         print("Generated password:", password)
 
-    with subtest("Check that IMAP authentication works:"):
+    with subtest("Check that IMAP authentication works"):
         server.succeed(f"curl -vvvvvvv --no-progress-meter imap://localhost:143/ -u vsh:{password}")
+
+    with subtest("Check that Dovecot delivers incoming mail properly"):
+        status = server.succeed(f"curl --no-progress-meter imap://localhost:143 -u vsh:{password} -X 'STATUS INBOX (MESSAGES)'").strip()
+        if status != "* STATUS INBOX (MESSAGES 0)":
+            raise Exception(f"Mailbox not empty: {status}")
+        server.succeed("cat ${test-email} | $(dirname $(readlink -f $(which dovecot)))/../libexec/dovecot/dovecot-lda -d vsh -e")
+        status = server.succeed(f"curl --no-progress-meter imap://localhost:143 -u vsh:{password} -X 'STATUS INBOX (MESSAGES)'").strip()
+        if status != "* STATUS INBOX (MESSAGES 1)":
+            raise Exception(f"Mail wasn't delivered: {status}")
+        # Strip carriage returns to aid comparison
+        mail = server.succeed(f"curl --no-progress-meter 'imap://localhost:143/INBOX;UID=1' -u vsh:{password}").replace("\r\n", "\n")
+        with open("${test-email}") as f:
+            template = f.read()
+        print("Mail sent:     ", repr(template))
+        print("Mail delivered:", repr(mail))
+        if mail != template:
+            raise Exception("Mail doesn't match what was sent")
   '';
 }
